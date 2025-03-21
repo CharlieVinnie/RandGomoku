bd9466d6f2c8306fe3d78bf290b01e49a0a57ab9
Charlie <charliewang.cpp@gmail.com>
2025-03-21 12:32:38 +0800

    push button

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QPushButton
 
 import sys
 
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = QPushButton("Push Me")
 window.show()
 
 app.exec()
