bc7fc39839c6248d7aeb72188700ecbd70964af1
Charlie <charliewang.cpp@gmail.com>
2025-03-21 12:37:57 +0800

    My first button

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from PyQt6.QtWidgets import QApplication, QPushButton
-
 import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+# from PyQt6.QtCore import QSize, Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press me!")
+
+        self.setCentralWidget(button)
+
 
 app = QApplication(sys.argv)
 
-window = QPushButton("Push Me")
+window = MainWindow()
 window.show()
 
 app.exec()
